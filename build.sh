a7bea50be6de147e21c87ad1739176ef6ab6e3e2
Thomas Szteliga <ts@websafe.pl>
2015-10-14 17:32:41 +0200

    Added Inkscape, LibreCAD, FreeCAD, whyteboard

--- build.sh
+++ build.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-#
 set -e
 
-# VLC
+## VLC
 if ! grep -q ^avahi: /etc/group; then
   groupadd -g 214 avahi
 fi
@@ -11,3 +10,20 @@ if ! grep -q ^avahi: /etc/passwd; then
   useradd -u 214 -g avahi -c Avahi -d /dev/null -s /bin/false avahi
 fi
 echo Y | slpkg -s sbo vlc
+
+## Inkscape
+echo Y | slpkg -s sbo inkscape
+
+## LibreCAD
+echo Y | slpkg -s sbo librecad
+
+## FreeCAD
+echo Y | slpkg -s sbo FreeCAD
+
+## whyteboard
+# libgnomeprintui is needed when building wxPython with GNOMEPRINT=yes
+echo Y | slpkg -s sbo libgnomeprintui
+# whyteboard needs wxPython built with GNOMEPRINT=yes
+export GNOMEPRINT=yes
+echo Y | slpkg -s sbo whyteboard
+
